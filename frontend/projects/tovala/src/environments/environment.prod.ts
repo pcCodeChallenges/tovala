46591c21bbd63b44d53a5cf7af6eca5addb7dc95
Pete Clodi <pete.clodi@gmail.com>
2020-03-26 04:07:19 -0500

    Fix environment file
    The Production environment file did not have the `firebaseConfig` property defined

--- frontend/projects/tovala/src/environments/environment.prod.ts
+++ frontend/projects/tovala/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig: {
+      apiKey: 'AIzaSyBIZPMjRTad8khUWXZQAiFUuH5jqenPVIA',
+      authDomain: 'tovala-pete-fb.firebaseapp.com',
+      databaseURL: 'https://tovala-pete-fb.firebaseio.com',
+      projectId: 'tovala-pete-fb',
+      storageBucket: 'tovala-pete-fb.appspot.com',
+      messagingSenderId: '883970168537',
+      appId: '1:883970168537:web:941103127bffad846e10c8'
+    }
 };
