82c2b8facad62b86a6a7737ec5921a688c753522
Pete Clodi <pete.clodi@gmail.com>
2020-03-28 12:20:02 -0500

    DnD with inner values updating now works

--- frontend/projects/tovala/src/app/layouts/box/box.ts
+++ frontend/projects/tovala/src/app/layouts/box/box.ts
@@ -2,11 +2,38 @@ import { Point } from '@angular/cdk/drag-drop/drag-ref';
 import { IDimensions } from '../dimensions/dimensions.interface';
 
 export class Box implements IDimensions {
+    private static NextBoxId: number = 0;
+
+    // Unique Identifier for this box object
+    id: number;
+
+    backgroundColor: string;
     height: number;
     topLeft: Point;
     width: number;
 
-   constructor(dimensions? :IDimensions) {
+    get serializedConfig(): string {
+        return JSON.stringify({
+            backgroundColor: this.backgroundColor,
+            height: this.height,
+            topLeft: this.topLeft,
+            width: this.width
+        });
+    }
+
+    constructor(dimensions? :IDimensions) {
         Object.assign(this, dimensions);
+
+        // Create a "unique" Id for each box
+        // This format allows for a maximum of
+        // ~9 quadrillion (based upon the
+        // IEEE 754 spec). So I think this will
+        // be a safe option
+        this.id = ++Box.NextBoxId;
+    }
+
+    calculateNewPosition(distance: Point) {
+        this.topLeft.x += distance.x;
+        this.topLeft.y += distance.y;
     }
 }
